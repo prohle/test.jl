c8e1edd3780357fb275a9b9133a81c7b8b297b48
Huong Yen Le <prohle@gmail.com>
2021-06-08 22:02:22 -0400

    man from documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,70 @@
-#make.jl
-push!(LOAD_PATH, "../src/")
+using Documenter, DocumenterTools
+include("DocumenterShowcase.jl")
 
-using Documenter, test
+# The DOCSARGS environment variable can be used to pass additional arguments to make.jl.
+# This is useful on CI, if you need to change the behavior of the build slightly but you
+# can not change the .travis.yml or make.jl scripts any more (e.g. for a tag build).
+if haskey(ENV, "DOCSARGS")
+    for arg in split(ENV["DOCSARGS"])
+        (arg in ARGS) || push!(ARGS, arg)
+    end
+end
 
 makedocs(
-    sitename = "test.jl",
-    modules = [test],
-    pages=[
-        "Home" => "index.md"
-    ]
+    modules = [Documenter, DocumenterTools, DocumenterShowcase],
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = !("local" in ARGS),
+        canonical = "https://prohle.github.io/test.jl/stable/",
+        assets = ["assets/favicon.ico"],
+        analytics = "UA-136089579-2",
+        highlights = ["yaml"],
+    ),
+    clean = false,
+    sitename = "Documenter.jl",
+    authors = "Michael Hatherly, Morten Piibeleht, and contributors.",
+    linkcheck = "linkcheck" in ARGS,
+    linkcheck_ignore = [
+        # timelessrepo.com seems to 404 on any CURL request...
+        "http://timelessrepo.com/json-isnt-a-javascript-subset",
+        # We'll ignore links that point to GitHub's edit pages, as they redirect to the
+        # login screen and cause a warning:
+        r"https://github.com/([A-Za-z0-9_.-]+)/([A-Za-z0-9_.-]+)/edit(.*)",
+    ] ∪ (get(ENV, "GITHUB_ACTIONS", nothing)  == "true" ? [
+        # Extra ones we ignore only on CI.
+        #
+        # It seems that CTAN blocks GitHub Actions?
+        "https://ctan.org/pkg/minted",
+    ] : []),
+    pages = [
+        "Home" => "index.md",
+        "Manual" => Any[
+            "Guide" => "man/guide.md",
+            "man/examples.md",
+            "man/syntax.md",
+            "man/doctests.md",
+            "man/latex.md",
+            hide("man/hosting.md", [
+                "man/hosting/walkthrough.md"
+            ]),
+            "man/other-formats.md",
+        ],
+        "showcase.md",
+        "Library" => Any[
+            "Public" => "lib/public.md",
+            "Internals" => map(
+                s -> "lib/internals/$(s)",
+                sort(readdir(joinpath(@__DIR__, "src/lib/internals")))
+            ),
+        ],
+        "contributing.md",
+    ],
+    strict = !("strict=false" in ARGS),
+    doctest = ("doctest=only" in ARGS) ? :only : true,
 )
 
-
-deploydocs(;
-    repo="github.com/prohle/test.jl",
+deploydocs(
+    repo = "prohle.github.com/test.jl.git",
+    target = "build",
+    push_preview = true,
 )
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "test.jl" begin
     # Write your tests here.
+    x = 2
+    y = 2
+    @test test.sum_values(x,y) == 4
 end
